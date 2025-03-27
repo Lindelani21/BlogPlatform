7d46e44c4dd812e8da8bf1363ed4dc972d63bc5e
Lindelani21 <lindelanilethabo21@gmail.com>
2025-03-27 16:11:03 +0200

    Creating Models and implementing them in the .Core

---
+++ BlogPlatform/BlogPlatform.Core/Models/BlogPost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPlatform.Core.Models
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string UserId { get; set; }
+        public User Author { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+        public ICollection<Like> Likes { get; set; }
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
---
+++ BlogPlatform/BlogPlatform.Core/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogPlatform.Core.Models
+{
+    public class User 
+    {
+        public string DisplayName { get; set; }
+        public string Bio { get; set; }
+        public ICollection<BlogPost> Posts { get; set; }
+        public ICollection<Comment> Comments { get; set; }
+        public ICollection<Like> Likes { get; set; }
+    }
+}
