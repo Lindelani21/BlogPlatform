1dd2acf897d20f8b1bbe6a4e254febd053a171f8
Lindelani21 <lindelanilethabo21@gmail.com>
2025-03-31 16:21:07 +0200

    Updating imports for the models in the API

--- BlogPlatform/BlogPlatform.API/Program.cs
+++ BlogPlatform/BlogPlatform.API/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
+builder.Services.AddScoped<BlogPostRepository>();
 
 //builder.Services.AddIdentity<User, IdentityRole>()
 //    .AddEntityFrameworkStores<BlogContext>()
--- BlogPlatform/BlogPlatform.Infrastructure/Repositories/BlogPostRepository.cs
+++ BlogPlatform/BlogPlatform.Infrastructure/Repositories/BlogPostRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BlogPlatform.Core.Models;
+using BlogPlatform.Infrastructure.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,21 +8,21 @@ using System.Threading.Tasks;
 
 namespace BlogPlatform.Infrastructure.Repositories
 {
-    // BlogPostRepository.cs
-    //public class BlogPostRepository
-    //{
-    //    private readonly BlogContext _context;
 
-    //    public BlogPostRepository(BlogContext context)
-    //    {
-    //        _context = context;
-    //    }
+    public class BlogPostRepository
+    {
+        private readonly BlogContext _context;
 
-    //    public async Task<List<BlogPost>> GetAllAsync()
-    //    {
-    //        return await _context.Posts
-    //            .Include(p => p.Author)
-    //            .ToListAsync();
-    //    }
-    //}
+        public BlogPostRepository(BlogContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<BlogPost>> GetAllAsync()
+        {
+            return await _context.Posts
+                .Include(p => p.Author)
+                .ToListAsync();
+        }
+    }
 }
